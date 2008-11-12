a76f4c371e4c81af48ad03b58d1fc04a6937b58b
Gerrit Pusher <pusher@localhost>
2008-11-13 00:48:49 +0800

    Import sqlite-amalgamation v3.6.5

--- shell.c
+++ shell.c
@@ -12,7 +12,7 @@
 ** This file contains code to implement the "sqlite" command line
 ** utility for accessing SQLite databases.
 **
-** $Id: shell.c,v 1.185 2008/08/11 19:12:35 drh Exp $
+** $Id: shell.c,v 1.187 2008/11/11 18:55:04 drh Exp $
 */
 #include <stdlib.h>
 #include <string.h>
@@ -1699,7 +1699,7 @@ static int process_input(struct callback_data *p, FILE *in){
       }
       continue;
     }
-    if( _is_command_terminator(zLine) ){
+    if( _is_command_terminator(zLine) && sqlite3_complete(zSql) ){
       memcpy(zLine,";",2);
     }
     nSqlPrior = nSql;
@@ -1757,7 +1757,7 @@ static int process_input(struct callback_data *p, FILE *in){
     }
   }
   if( zSql ){
-    if( !_all_whitespace(zSql) ) printf("Incomplete SQL: %s\n", zSql);
+    if( !_all_whitespace(zSql) ) fprintf(stderr, "Incomplete SQL: %s\n", zSql);
     free(zSql);
   }
   free(zLine);
