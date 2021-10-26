271e72f16ffbcb77584760b223ecef97a345a2f0
Ger Hobbelt <ger@hobbelt.com>
2021-10-26 02:27:40 +0200

    fix compiler warning about using potentially unused var. Just shutting up the compiler here ...

--- test/threadtest4.c
+++ test/threadtest4.c
@@ -251,7 +251,7 @@ static void worker_delete_all_content(WorkerInfo *p, int inTrans){
 ** Create rows mn through mx in table iTab for the given worker
 */
 static void worker_add_content(WorkerInfo *p, int mn, int mx, int iTab){
-  char *zTabDef;
+  char *zTabDef = NULL;
   switch( iTab ){
     case 1:  zTabDef = "t1(tid,sp,a,b,c)";  break;
     case 2:  zTabDef = "t2(tid,sp,d,e,f)";  break;
